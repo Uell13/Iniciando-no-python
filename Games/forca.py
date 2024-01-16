c2626aa8a5e4efbae2110ad4918e38cd09805bfe
Raphael_Netoo <rafaelneto3047@gmail.com>
2024-01-16 06:30:07 -0300

    Update forca.py

--- Games/forca.py
+++ Games/forca.py
@@ -3,11 +3,12 @@
 
 def jogar():
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_corretas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     # len() retorna a quantidade de elementos em uma lista ou a quantidade de caracteres em uma frase
     print(f"\nA palavra secreta tem {len(letras_corretas)} letras!")
@@ -16,25 +17,29 @@ def jogar():
     while (not enforcou and not acertou):
 
         # strip() -> Remove os espaços da string
-        chute = input("\nEscolha uma letra: ").strip()
+        chute = input("\nEscolha uma letra: ").strip().upper()
         
-        posicao = 0
+        if (chute in palavra_secreta):
+            posicao = 0
 
-        # Esse FOR percorre toda a string e separa as posições de cada letra da palavra secreta
-        for letra in palavra_secreta:
+            # Esse FOR percorre toda a string e separa as posições de cada letra da palavra secreta
+            for letra in palavra_secreta:
 
-            # lower() -> Converte toda a string para o formato minúsculo
-            # upper() -> Converte toda a string para o formato maiusculo
-            if (chute.lower() == letra.lower()):
-                letras_corretas[posicao] = letra
-            posicao += 1
+                # lower() -> Converte toda a string para o formato minúsculo
+                # upper() -> Converte toda a string para o formato maiusculo
+                if (chute == letra):
+                    letras_corretas[posicao] = letra
+                posicao += 1
 
-        if (chute.lower() in letras_corretas):
-            print(f"\nNice! Você acertou {letras_corretas.count(chute)} letra(s).")
-            print("Veja como ficou a palavra secreta:")
+            if (chute in letras_corretas):
+                print(f"\nNice! Você acertou {letras_corretas.count(chute)} letra(s).")
+                print("Veja como ficou a palavra secreta:")
         else:
-            print("Que pena. Não achei essa letra na palavra secreta!")
+            erros += 1
+            print(f"Ops, você errou! Faltam {6-erros} tentativas.")
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_corretas
         print(letras_corretas)
         print(f"Quantidade de letras ocultas: {letras_corretas.count("_")}")
 
